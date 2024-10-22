c6f8560bfe93d5b009a8472d3c185d869fbe11d8
Rawen Ben Trad <bentradrawen@gmail.com>
2024-10-22 18:09:28 +0100

    prosit 1 instruction 1

---
+++ src/ZooManagement.java
@@ -0,0 +1,10 @@
+
+public class ZooManagement {
+    public static void main(String[] args) {
+       //prosit1 instruction 1
+        int nbrCages =20 ;
+        String zooName ="myzoo";
+        System.out.println( zooName +" comporte " + nbrCages +" Cages ");
+        
+    }
+}
