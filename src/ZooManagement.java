89e9693b5bea563a488eab33161809ffd5c68f9c
Rawen Ben Trad <bentradrawen@gmail.com>
2024-10-22 18:20:28 +0100

    instruction 2 et 3

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 
 public class ZooManagement {
+
+    //instruction2 utilisation scanner
+
+    int nbrCages ;
+    String zooName ;
     public static void main(String[] args) {
-       //prosit1 instruction 1
-        int nbrCages =20 ;
-        String zooName ="myzoo";
-        System.out.println( zooName +" comporte " + nbrCages +" Cages ");
-        
+
+        ZooManagement zoo = new ZooManagement();
+
+
+        System.out.println( zoo.zooName +" comporte " + zoo.nbrCages +" Cages ");
+         Scanner sc=new Scanner(System.in);
+        System.out.print("Entrez le nom du zoo : ");
+        zoo.zooName= sc.nextLine();
+
+        System.out.print("Entrez le nombre de cages : ");
+        zoo.nbrCages = sc.nextInt();
+        System.out.println(zoo.zooName +" comporte " + zoo.nbrCages +" Cages ");
+
     }
 }
