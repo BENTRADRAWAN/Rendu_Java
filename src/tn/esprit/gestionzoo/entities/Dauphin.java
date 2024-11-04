d5889baa49507fb2b4f7aa919e856833f0fd4d4d
Rawen Ben Trad <bentradrawen@gmail.com>
2024-11-04 21:56:44 +0100

    prosit 5 instruction 20

---
+++ src/tn/esprit/gestionzoo/entities/Dauphin.java
@@ -0,0 +1,9 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Dauphin  extends Aquatic{
+    protected float swimmingDepth;
+
+    public Dauphin(String family, String name, int age, boolean isMammal) {
+        super(family, name, age, isMammal);
+    }
+}
---
+++ src/tn/esprit/gestionzoo/entities/Penguin.java
@@ -0,0 +1,9 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Penguin  extends Aquatic{
+    protected float swimmingSpeed;
+
+    public Penguin(String family, String name, int age, boolean isMammal) {
+        super(family, name, age, isMammal);
+    }
+}
